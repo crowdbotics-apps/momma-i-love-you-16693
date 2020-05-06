1e697edb0d6afecd6959438adb1911e903146fc0
Crowdbotics <team@crowdbotics.com>
2020-05-06 22:27:51 +0000

    Update models

--- backend/momma_i_love_you_16693/wsgi.py
+++ backend/momma_i_love_you_16693/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'momma_i_love_you_16693.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "momma_i_love_you_16693.settings")
 
 application = get_wsgi_application()
